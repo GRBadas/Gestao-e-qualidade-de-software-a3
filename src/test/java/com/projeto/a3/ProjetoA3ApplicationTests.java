4888c90e4663f27cdb28bb6c7aa0a4e2ddbac094
Badas <grbadas@gmail.com>
2024-03-27 02:11:32 -0300

    code

---
+++ src/test/java/com/projeto/a3/ProjetoA3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.a3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoA3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
