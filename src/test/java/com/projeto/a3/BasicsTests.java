04f51a4d13c17bc22a28d060351e653a78ed18c4
grbadas <grbadas@gmail.com>
2024-04-02 20:39:53 -0300

    test "Primeiro número menor que o segundo"

--- src/test/java/com/projeto/a3/BasicsTests.java
+++ src/test/java/com/projeto/a3/BasicsTests.java
@@ -1,6 +1,7 @@
 package com.projeto.a3;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BasicsTests {
@@ -9,5 +10,13 @@ public class BasicsTests {
         Basics basicTests = new Basics();
         int value = basicTests.compare(2,1);
         Assertions.assertEquals(1, value);
+    }
+
+    @Test
+    @DisplayName("Primeiro número menor que o segundo")
+    public void compare2() {
+        Basics basicTests = new Basics();
+        int value = basicTests.compare(2, 3);
+        Assertions.assertEquals(-1, value);
     }
 }
